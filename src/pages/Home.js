b90504e0ce6f81710e65006ab62387ab6756a22c
Gaurav konde <63660013+Gauravk2610@users.noreply.github.com>
2022-08-11 04:28:07 +0530

    condition checking added

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -102,7 +102,7 @@ const Home = () => {
                     accept='image/x-png,image/gif,image/jpeg, image/jpg' />
                     <img 
                     onClick={() => imgRef.current.click()}
-                    src={userDetails.photoURL || userImg ? userImg || userDetails.photoURL : 'https://i1.wp.com/wilcity.com/wp-content/uploads/2020/06/115-1150152_default-profile-picture-avatar-png-green.jpg?fit=820%2C860&ssl=1'} 
+                    src={userDetails?.photoURL || userImg ? userImg || userDetails?.photoURL : 'https://i1.wp.com/wilcity.com/wp-content/uploads/2020/06/115-1150152_default-profile-picture-avatar-png-green.jpg?fit=820%2C860&ssl=1'} 
                     alt='Profile' className='w-full h-full object-cover rounded-full' />
                 </div>
                 {/* First Name */}
